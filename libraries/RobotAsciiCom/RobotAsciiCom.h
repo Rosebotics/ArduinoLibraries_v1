7b35d9579f46062deaae619dee6cb699ff53eece
David Fisher <fisherds@rose-hulman.edu>
2014-06-11 00:37:33 -0400

    Begin making RobotAsciiCom
    Started with only a PWM command and a few others that didn’t change
    from ME435.  Still untested as I don’t have other libraries complete.

--- libraries/RobotAsciiCom/RobotAsciiCom.h
+++ libraries/RobotAsciiCom/RobotAsciiCom.h
@@ -6,38 +6,25 @@
 #define MAX_ASCII_MESSAGE_LENGTH 	64 // If you have longer message change this!
 #define MESSAGE_TERMINATOR 			10
 
-// Constants for WHEEL SPEED messages.
-#define WHEEL_SPEED_MODE_REVERSE 0  // Mode to go in reverse
-#define WHEEL_SPEED_MODE_BRAKE   1  // Mode to stop
-#define WHEEL_SPEED_MODE_FORWARD 2  // Mode to go forward
 
-class RobotAsciiCom
-{
+class RobotAsciiCom {
   public:
     RobotAsciiCom();
     void handleRxByte(byte newRxByte);
     void handleRxBytes(byte newRxBytes[], int length);
-    void registerPositionCallback(void (* positionCallback)(int joint1Angle, int joint2Angle, int joint3Angle, int joint4Angle, int joint5Angle) );
-    void registerJointAngleCallback(void (* jointAngleCallback)(byte jointNumber, int jointAngle) );
-    void registerGripperCallback(void (* gripperCallback)(int gripperDistance) );
-    void registerAttachSelectedServosCallback( void (* attachSelectedServosCallback)(byte servosToEnable) );
-	void registerWheelSpeedCallback(void (* wheelSpeedCallback)(byte leftMode, byte rightMode, byte leftDutyCycle, byte rightDutyCycle) );
-    void registerBatteryVoltageRequestCallback(void (* batteryVoltageRequestCallback)(void) );
-    void registerWheelCurrentRequestCallback(void (* wheelCurrentRequestCallback)(void) );
+	void registerPwmCallback(void (* pwmCallback)(int leftDutyCycle, int rightDutyCycle) );
 	void registerCustomStringCallback( void (* customStringCallback)(String customString) );
+    void registerBatteryVoltageRequestCallback(void (* batteryVoltageRequestCallback)(void) );
+    void registerCurrentRequestCallback(void (* currentRequestCallback)(void) );
 	int prepareBatteryVoltageReply(int batteryMillivolts, char buf[], int maxLength);
-	int prepareWheelCurrentReply(int leftWheelMotorsMilliamps, int rightWheelMotorsMilliamps, char buf[], int maxLength);
+	int prepareCurrentReply(int leftMotorMilliamps, int rightMotorMilliamps, char buf[], int maxLength);
   private:
     char _rxMessageBuffer[MAX_ASCII_MESSAGE_LENGTH];
     int _nextOpenByteInMessageBuffer;
-    void (* _positionCallback)(int joint1Angle, int joint2Angle, int joint3Angle, int joint4Angle, int joint5Angle);
-    void (* _jointAngleCallback)(byte joint, int jointAngle);
-    void (* _gripperCallback)(int gripperDistance);
-    void (* _attachSelectedServosCallback)(byte servosToEnable);
-    void (* _wheelSpeedCallback)(byte leftMode, byte rightMode, byte leftDutyCycle, byte rightDutyCycle);
-    void (* _batteryVoltageRequestCallback)(void);
-    void (* _wheelCurrentRequestCallback)(void);
+    void (* _pwmCallback)(int leftDutyCycle, int rightDutyCycle);
     void (* _customStringCallback)(String customString);
+    void (* _batteryVoltageRequestCallback)(void);
+    void (* _currentRequestCallback)(void);
     void _parseStringCommand(String command);
 };
 
--- libraries/RobotAsciiCom/examples/UnitTesting/UnitTesting.ino
+++ libraries/RobotAsciiCom/examples/UnitTesting/UnitTesting.ino
@@ -3,82 +3,31 @@
 RobotAsciiCom robotCom;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(100);
-  robotCom.registerPositionCallback(positionCallback);
-  robotCom.registerJointAngleCallback(jointAngleCallback);
-  robotCom.registerGripperCallback(gripperCallback);
   robotCom.registerBatteryVoltageRequestCallback(batteryVoltageRequestCallback);
-  robotCom.registerWheelSpeedCallback(wheelSpeedCallback);
+  robotCom.registerCurrentRequestCallback(currentRequestCallback);
+  robotCom.registerPwmCallback(pwmCallback);
   robotCom.registerCustomStringCallback(customStringCallback);
-  robotCom.registerAttachSelectedServosCallback(attachSelectedServosCallback);
-}
-
-void positionCallback(int joint1Angle, int joint2Angle, int joint3Angle, int joint4Angle, int joint5Angle) {
-  Serial.println();
-  Serial.print("Position command: ");
-  Serial.print(joint1Angle);
-  Serial.print(" ");
-  Serial.print(joint2Angle);
-  Serial.print(" ");
-  Serial.print(joint3Angle);
-  Serial.print(" ");
-  Serial.print(joint4Angle);
-  Serial.print(" ");
-  Serial.print(joint5Angle);
-  Serial.println();
-}
-
-void jointAngleCallback(byte jointNumber, int jointAngle) {
-  Serial.println();
-  Serial.print("Joint angle command for joint ");
-  Serial.print(jointNumber);
-  Serial.print(" to move to ");
-  Serial.print(jointAngle);
-  Serial.println();
-}
-
-void gripperCallback(int gripperDistance) {
-  Serial.println();
-  Serial.print("Gripper command to ");
-  Serial.print(gripperDistance);
-  Serial.println();
-
 }
 
 void batteryVoltageRequestCallback(void) {
   Serial.println();
   Serial.print("Battery voltage request");
   Serial.println();
-
 }
 
-void wheelSpeedCallback(byte leftMode, byte rightMode, byte leftDutyCycle, byte rightDutyCycle) {
+void currentRequestCallback(void) {
   Serial.println();
-  Serial.print("Wheel speed command.  ");
-  switch (leftMode) {
-    case WHEEL_SPEED_MODE_REVERSE:
-      Serial.print("Left Reverse ");
-      break;
-    case WHEEL_SPEED_MODE_BRAKE:
-      Serial.print("Left Brake ");
-      break;
-    case WHEEL_SPEED_MODE_FORWARD:
-      Serial.print("Left Forward ");
-      break;
-  }
+  Serial.print("Current request");
+  Serial.println();
+}
+
+void pwmCallback(int leftDutyCycle, int rightDutyCycle) {
+  Serial.println();
+  Serial.print("PWM command.  ");
   Serial.print(leftDutyCycle);
-  switch (rightMode) {
-    case WHEEL_SPEED_MODE_REVERSE:
-      Serial.print(" Right Reverse ");
-      break;
-    case WHEEL_SPEED_MODE_BRAKE:
-      Serial.print(" Right Brake ");
-      break;
-    case WHEEL_SPEED_MODE_FORWARD:
-      Serial.print(" Right Forward ");
-      break;
-  }
+  Serial.print("  ");
   Serial.print(rightDutyCycle);  
   Serial.println();
 }
@@ -88,15 +37,8 @@ void customStringCallback(String customString) {
   Serial.println();
 }
 
-void attachSelectedServosCallback(byte servosToEnable) {
-  Serial.print("Attach: ");  
-  Serial.print(servosToEnable, BIN);  
-  Serial.println();
-}
 
-
-void loop() {
-  
+void loop() {  
   Serial.print("Test 1: ");
   test1();
   Serial.println("");
@@ -125,58 +67,27 @@ void loop() {
   test9();
   Serial.println("");
   // Output should be... (ignoring line breaks)
-  // Test 1: Gripper command to 42
-  // Test 2: Position command: 10 20 30 40 50
+  // Test 1: Pwm command. 123  -80
+  // Test 2: 
   // Test 3: Battery voltage request
-  // Test 4: Joint angle command for joint 3 to move to 33
-  // Test 5: Wheel speed command.  Left Forward 120 Right Brake 4
-  // Test 6: Joint angle command for joint 1 to move to -88
+  // Test 4: 
+  // Test 5: 
+  // Test 6: 
   // Test 7: Hello, World!
   // Test 8: 
-  // Test 9: Attach: 111110
+  // Test 9: 
   
   while(1);
 }
 
 void test1() {
-  robotCom.handleRxByte('G');
-  robotCom.handleRxByte('R');
-  robotCom.handleRxByte('I');
-  robotCom.handleRxByte('P');
-  robotCom.handleRxByte('P');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte('R');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('4');
-  robotCom.handleRxByte('2');
-  robotCom.handleRxByte('\n');
+  byte testArray[] = "PWM 123 -80";
+  testArray[17] = '\n';
+  robotCom.handleRxBytes(testArray, 18);
 }
 
 void test2() {
-  robotCom.handleRxByte('P');
-  robotCom.handleRxByte('O');
-  robotCom.handleRxByte('S');
-  robotCom.handleRxByte('I');
-  robotCom.handleRxByte('T');
-  robotCom.handleRxByte('I');
-  robotCom.handleRxByte('O');
-  robotCom.handleRxByte('N');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('1');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('2');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('3');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('4');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('5');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte('\n');
+
 }
 
 void test3() {
@@ -207,64 +118,12 @@ void test3() {
 }
 
 void test4() {
-  robotCom.handleRxByte('J');
-  robotCom.handleRxByte('O');
-  robotCom.handleRxByte('I');
-  robotCom.handleRxByte('N');
-  robotCom.handleRxByte('T');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('3');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('A');
-  robotCom.handleRxByte('N');
-  robotCom.handleRxByte('G');
-  robotCom.handleRxByte('L');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('3');
-  robotCom.handleRxByte('3');
-  robotCom.handleRxByte('\n');
 }
 
 void test5() {
-  robotCom.handleRxByte('W');
-  robotCom.handleRxByte('H');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte('L');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('S');
-  robotCom.handleRxByte('P');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte('D');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('F');
-  robotCom.handleRxByte('O');
-  robotCom.handleRxByte('R');
-  robotCom.handleRxByte('W');
-  robotCom.handleRxByte('A');
-  robotCom.handleRxByte('R');
-  robotCom.handleRxByte('D');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('1');
-  robotCom.handleRxByte('2');
-  robotCom.handleRxByte('0');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('B');
-  robotCom.handleRxByte('R');
-  robotCom.handleRxByte('A');
-  robotCom.handleRxByte('K');
-  robotCom.handleRxByte('E');
-  robotCom.handleRxByte(' ');
-  robotCom.handleRxByte('4');
-  robotCom.handleRxByte('\n');
 }
 
 void test6() {
-  byte testArray[] = "JOINT 1 ANGLE -88 ";
-  testArray[17] = '\n';
-  robotCom.handleRxBytes(testArray, 18);
 }
 
 void test7() {
@@ -274,13 +133,7 @@ void test7() {
 }
 
 void test8() {
-  byte testArray[] = "ATTACH 1101 ";
-  testArray[11] = '\n';
-  robotCom.handleRxBytes(testArray, 12);
 }
 
 void test9() {
-  byte testArray[] = "ATTACH 111110 ";
-  testArray[13] = '\n';
-  robotCom.handleRxBytes(testArray, 14);
 }
