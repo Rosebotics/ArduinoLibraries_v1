fd20657e769816ec257f710d9e9ec68cc0b4b112
David Fisher <fisherds@rose-hulman.edu>
2014-06-11 00:44:46 -0400

    Update the Simple commands to only PWM

--- libraries/RobotAsciiCom/examples/SimpleCommands/SimpleCommands.ino
+++ libraries/RobotAsciiCom/examples/SimpleCommands/SimpleCommands.ino
@@ -1,36 +1,21 @@
-#include <Servo.h>
-#include <ArmServos.h>
 #include <RobotAsciiCom.h>
 
-ArmServos armServos;
 RobotAsciiCom robotCom;
 
 void setup() {
-  Serial.begin(9600);
-  delay(100);
-  armServos.attach();
-  robotCom.registerPositionCallback(positionCallback);
-  robotCom.registerJointAngleCallback(jointAngleCallback);
-  robotCom.registerGripperCallback(gripperCallback);
+  Serial.begin(115200);
+  robotCom.registerPwmCallback(pwmCallback);
 }
 
-void positionCallback(int joint1Angle, int joint2Angle, int joint3Angle, int joint4Angle, int joint5Angle) {
-  armServos.setPosition(joint1Angle, joint2Angle, joint3Angle, joint4Angle, joint5Angle);
-}
-
-void jointAngleCallback(byte jointNumber, int jointAngle) {
-  armServos.setJointAngle(jointNumber, jointAngle);
-}
-
-void gripperCallback(int gripperDistance) {
-  armServos.setGripperDistance(gripperDistance);
+void pwmCallback(int leftDutyCycle, int rightCutyCycle) {
+  // TODO: Carry out the request using a libraries instead of hardcoding.
 }
 
 void loop() {
 }
 
 
-/** Send all bytes received to the Wild Thumper Com object. */
+/** Send all bytes received to the RobotAsciiCom object. */
 void serialEvent() {
   while (Serial.available()) {
     robotCom.handleRxByte(Serial.read());
