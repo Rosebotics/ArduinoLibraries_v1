6598ea4a9e969c41ab98c8d4248c9bef93704d70
David Fisher <fisherds@rose-hulman.edu>
2015-04-14 11:49:18 -0400

    Added a pushbutton to make the robot go

--- libraries/MiniQCom/examples/EdisonTx_DrivePwm/EdisonTx_DrivePwm.ino
+++ libraries/MiniQCom/examples/EdisonTx_DrivePwm/EdisonTx_DrivePwm.ino
@@ -1,14 +1,23 @@
 #include <Wire.h>
 #include <MiniQCom.h>
 
+#define PIN_PUSHBUTTON 7
+#define PRESSED 0 
+
 #define MINI_Q_ADDRESS 4
 MiniQCom miniQCom(true, MINI_Q_ADDRESS);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(PIN_PUSHBUTTON, INPUT_PULLUP);
   Wire.begin(); // No address required for the master.
   
-  // Start with a forward back.
+  // Start the program with a 
+  sendForwardBack();
+}
+
+void sendForwardBack() {
+   // Start with a forward back.
   miniQCom.sendDrivePwm(1 ,1,200, 200); // Forwards
   delay(2000);
   miniQCom.sendDrivePwm(1, 1, 0, 0);     // Stop
@@ -18,8 +27,12 @@ void setup() {
   miniQCom.sendDrivePwm(0, 0, 0, 0);     // Stop
 }
 
-
 void loop() {
+  if (digitalRead(PIN_PUSHBUTTON) == PRESSED) {
+    sendForwardBack();
+    delay(1000); // Simplified debounce mechanism
+  }
+  
   if (Serial.available()) {
     char newByte = Serial.read();
     if (newByte == 'f') {
